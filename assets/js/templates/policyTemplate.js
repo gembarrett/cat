90ba5caf10c7d12c0071277114c1bdc27430473c
Gem Barrett <gembarrett@users.noreply.github.com>
2021-01-19 22:50:04 -0500

    remove policy editing #5

--- assets/js/templates/policyTemplate.js
+++ assets/js/templates/policyTemplate.js
@@ -16,31 +16,9 @@ templates.policyTemplate = function(data){
       <div id="policy-dl" class="window pink-border-glow">
         <h3>Get policy</h3>
         <div class="dlBtnWrapper">
-          <p>Download pre-formatted versions of your organizational security policy documents in plaintext, markdown and HTML.</p>
+          <p>Download pre-formatted versions of your report in plaintext, markdown and HTML.</p>
         <div>`+ txt + md + html + `</div>
       </div>
-      </div>
-      <div id="policy-edit" class="window pink-border-glow">
-        <h3>Edit policy</h3>
-        <div>
-          <p>Edit your policy below for copying and pasting into your own file.</p>
-          <textarea class="policyHolder">`+docContent.plain+`</textarea>
-          <button class="btn btn-seco" onclick="resetChanges()">
-            <i class="fas fa-redo"></i> Reset changes
-          </button>
-          `+editTxt+`
-        </div>
-      </div>
-      <div class="window pink-border-glow">
-        <h3>What did you think?</h3>
-        <div>
-          <p>If you have a few minutes, it would be great to hear your thoughts on SOAP so it can be improved in the future. SOAP doesn't use analytics to follow you around the site, so this is the best way to share your opinion.</p>
-          <a class="btn btn-seco" href="https://feedback024968.typeform.com/to/ykpDnElo" target="_blank" title="Click to take a short survey about SOAP">Sure, I'll complete a quick survey!</a>
-        </div>
-      </div>
-      <div id="resources" class="window pink-border-glow">
-        <h3>Learn more</h3>`
-        +links+`</div>
       </div>
       <div id="policy-reset" class="window pink-border-glow">
         <h3>Start over</h3>
