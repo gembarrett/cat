ccb8e8425c7cf1d7085bb1358bb4458d1e59525a
Gem Barrett <gembarrett@users.noreply.github.com>
2021-01-20 22:30:36 -0500

    removed reliance on a list of non-text questions

--- assets/js/start.js
+++ assets/js/start.js
@@ -111,15 +111,11 @@ function enableSnapshot(s){
   if (currentState.answers.length !== 0){
     // check the ids against the checkableQs
     for (count = 0; count< currentState.answers.length; count++){
-      if (isCheckableQ(parseInt(currentState.answers[count].q))){
-        // enable the snapshot button
-        s.removeAttribute('disabled');
-        s.addEventListener('click', getSnapshotURL);
-        document.querySelector('#copyBtn').addEventListener('click', copyUrl);
-        return;
-      } else {
-        console.log(currentState.answers[count].q + ' is not a question that can be snapshotted');
-      }
+      // enable the snapshot button
+      s.removeAttribute('disabled');
+      s.addEventListener('click', getSnapshotURL);
+      document.querySelector('#copyBtn').addEventListener('click', copyUrl);
+      return;
     }
   } else {
     console.log('More answers needed for snapshotting');
