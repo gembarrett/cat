b6f06924655f5c155bc4910df09a56999b2f1dc8
Gem Barrett <gembarrett@users.noreply.github.com>
2022-03-04 12:57:36 -0500

    preparing json structure of question content

---
+++ content/en-content.js
@@ -0,0 +1,125 @@
+var qs = [
+  {
+    "level": "intro", // which of the three levels do these questions belong to
+    "questions": [
+      {
+        "q": "Question placeholder goes here",
+        "area": "a",
+        "answers": [
+          {
+            "a": "Answer placeholder goes here",
+            "pts": 10
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
+      },
+      {
+        "q": "Question placeholder goes here",
+        "area": "b",
+        "answers": [
+          {
+            "a": "Answer placeholder goes here",
+            "pts": 10
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
+      },
+    ]
+  },
+  {
+    "level": "basic",
+    "questions": [
+      {
+        "q":"Question placeholder goes here",
+        "area":"c",
+        "answers": [
+          {
+            "a": "Answer placeholder goes here",
+            "pts": 10
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
+      },
+      {
+        "q": "Question placeholder goes here",
+        "area": "c",
+        "answers": [
+          {
+            "a": "Answer placeholder goes here",
+            "pts": 10
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
+      },
+    ]
+  },
+  {
+    "level": "advanced",
+    "questions": [
+      {
+        "q":"Question placeholder goes here",
+        "area":"d",
+        "answers": [
+          {
+            "a": "Answer placeholder goes here",
+            "pts": 10
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
+      },
+      {
+        "q": "Question placeholder goes here",
+        "area": "d",
+        "answers": [
+          {
+            "a": "Answer placeholder goes here",
+            "pts": 10
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
+      },
+    ]
+  }
+]
