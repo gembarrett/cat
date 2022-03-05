09ee313a0b0a397fe7e6a0e9db362b6b2bb3287b
Gem Barrett <gembarrett@users.noreply.github.com>
2022-03-05 16:42:22 -0500

    prepared json structure for additional content

--- content/en-content.js
+++ content/en-content.js
@@ -223,4 +223,138 @@ var rs = [ // contains all evaluation content
       }
     ]
   }
+];
+
+var oc = [ // contains all other content
+  {
+    "type": "ux",
+    "title": "",
+    "content": [
+      {
+        "type": "button",
+        "text": "Placeholder label goes here"
+      },
+      {
+        "type": "direction",
+        "text": "Placeholder instruction goes here"
+      }
+    ]
+  },
+  {
+    "type": "glossary",
+    "title": "Glossary of Terms",
+    "content": [
+      {
+        "word": "Word placeholder goes here",
+        "define": "Definition of this word goes here"
+      },
+      {
+        "word": "Next word placeholder goes here",
+        "define": "Definition of this word goes here"
+      },
+      {
+        "word": "Another word placeholder goes here",
+        "define": "Definition of this word goes here"
+      },
+    ]
+  },
+  {
+    "type": "proj-intro",
+    "title": "Cybersecurity Assessment Tool (CAT)",
+    "content": [
+      {
+        "p": "Paragraph of text goes here"
+      },
+      {
+        "p": "Paragraph of text goes here"
+      }
+    ]
+  },
+  {
+    "type": "team-intro",
+    "title": "Who built this tool",
+    "content": [
+      {
+        "name": "Name placeholder goes here",
+        "p": "Paragraph about this person goes here"
+      },
+      {
+        "name": "Next name placeholder goes here",
+        "p": "Paragraph about this person goes here"
+      },
+      {
+        "name": "Another name placeholder goes here",
+        "p": "Paragraph about this person goes here"
+      }
+    ]
+  },
+  {
+    "type": "process",
+    "title": "How it works",
+    "content": [
+      {
+        "p": "Paragraph of text goes here"
+      },
+      {
+        "p": "Paragraph of text goes here",
+        "list": ["item 1", "item 2", "item 3", "item 4"]
+      },
+      {
+        "p": "Paragraph of text goes here"
+      },
+      {
+        "p": "Paragraph of text goes here"
+      }
+    ]
+  },
+  {
+    "type": "users",
+    "title": "Who did we build this tool for?",
+    "content": [
+      {
+        "p": "Paragraph of text goes here"
+        "list": [
+          "Long placeholder item 1 goes here",
+          "Long placeholder item 2 goes here",
+          "Long placeholder item 3 goes here",
+          "Long placeholder item 4 goes here",
+          "Long placeholder item 5 goes here",
+          "Long placeholder item 6 goes here",
+        ]
+      },
+      {
+        "p": "Paragraph of text goes here"
+      }
+    ]
+  },
+  {
+    "type": "legal",
+    "title": "Legal disclaimer",
+    "content": [
+      {
+        "p": "Paragraph of text goes here"
+      },
+      {
+        "p": "Paragraph of text goes here"
+      },
+      {
+        "p": "Paragraph of text goes here",
+        "list": [
+          "Long placeholder item 1 goes here",
+          "Long placeholder item 2 goes here",
+        ]
+      },
+      {
+        "p": "Paragraph of text goes here",
+        "list": [
+          "Long placeholder item 1 goes here",
+          "Long placeholder item 2 goes here",
+          "Long placeholder item 3 goes here",
+        ]
+      },
+      {
+        "p": "Paragraph of text goes here"
+      }
+    ]
+  }
 ]
