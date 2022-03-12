c25e39793de6373a94587a22cd3796e8ff29035b
Gem Barrett <gembarrett@users.noreply.github.com>
2022-03-11 21:55:58 -0500

    links now in (rough) alphabetical order

--- content/links.js
+++ content/links.js
@@ -1,4 +1,159 @@
 var links = {
+  "1pass": {
+    "biz": {
+      "en": "https://1password.com/business/"
+    },
+    "discount": {
+      "en": "https://1password.community/discussion/87059/teams-schools-and-other-nonprofits"
+    }
+  },
+  "2fa": {
+    "web": {
+      "en": "http://twofactorauth.org"
+    }
+  },
+  "3ma": {
+    "web": {
+      "en": "https://threema.ch/en"
+    }
+  },
+  "aclu": {
+    "stingrays": {
+      "en": "https://www.aclu.org/issues/privacy-technology/surveillance-technologies/stingray-tracking-devices-whos-got-them"
+    }
+  },
+  "an": {
+    "tmodel": {
+      "en": "https://www.accessnow.org/first-look-at-digital-security/"
+    }
+  },
+  "apache": {
+    "oofice": {
+      "en": "https://www.openoffice.org/"
+    }
+  },
+  "apple": {
+    "filevault": {
+      "en": "https://support.apple.com/en-us/HT204837"
+    },
+    "management": {
+      "en": "https://www.apple.com/business/essentials/"
+    }
+  },
+  "apricorn": {
+    "web": {
+      "en": "https://apricorn.com/"
+    }
+  },
+  "authy": {
+    "web": {
+      "en": "https://authy.com/features/"
+    }
+  },
+  "babelst": {
+    "web": {
+      "en": "https://www.babelstreet.com/"
+    }
+  },
+  "bitdefender": {
+    "gzultra": {
+      "en": "https://www.bitdefender.com/business/enterprise-products/ultra-security.html"
+    }
+  },
+  "bmask": {
+    "web": {
+      "en": "https://bitmask.net"
+    }
+  },
+  "bparty": {
+    "web": {
+      "en": "https://www.blockpartyapp.com/"
+    }
+  },
+  "bwarden": {
+    "web": {
+      "en": "https://bitwarden.com/"
+    }
+  },
+  "cat": {
+    "irplan": {
+      "en": "https://docs.google.com/document/d/1wf4il1JI5Bm7kI2JVo1diUv-v4A_a6Fk59NeNtrgoiA/edit?usp=sharing"
+    }
+  },
+  "carbide": {
+    "windows": {
+      "en": "https://carbidesecure.com/resources/security-best-practices-hardening-windows-10/"
+    },
+    "mac": {
+      "en": "https://carbidesecure.com/resources/our-best-practices-for-securing-your-macbook/"
+    }
+  },
+  "cflare": {
+    "ddos": {
+      "en": "https://www.cloudflare.com/learning/ddos/what-is-a-ddos-attack/"
+    },
+    "gal": {
+      "en": "https://www.cloudflare.com/galileo/"
+    },
+    "https": {
+      "en": "https://www.cloudflare.com/learning/ssl/what-is-https/"
+    }
+  },
+  "cisco": {
+    "webex": {
+      "en":"https://www.webex.com/"
+    }
+  },
+  "cmator": {
+    "web": {
+      "en": "https://cryptomator.org/"
+    }
+  },
+  "coral": {
+    "web": {
+      "en": "https://coralproject.net/"
+    }
+  },
+  "creports": {
+    "vpns": {
+      "en": "https://www.consumerreports.org/vpn-services/should-you-use-a-vpn-a5562069524/"
+    }
+  },
+  "ctangle": {
+    "web": {
+      "en": "https://www.crowdtangle.com/"
+    }
+  },
+  "ctower": {
+    "web": {
+      "en": "https://changetower.com/"
+    }
+  },
+  "deleteme": {
+    "web": {
+      "en": "https://joindeleteme.com/"
+    }
+  },
+  "dlane": {
+    "web": {
+      "en": "https://www.dashlane.com/"
+    }
+  },
+  "dnstwist": {
+    "web": {
+      "en": "https://dnstwist.it/"
+    }
+  },
+  "docebo": {
+    "web": {
+      "en": "https://www.docebo.com/"
+    }
+  },
+  "duo": {
+    "web": {
+      "en": "https://duo.com/"
+    }
+  },
   "eff": {
     "tmodel": {
       "en": "https://sec.eff.org/topics/threat-modeling"
@@ -13,159 +168,19 @@ var links = {
       "en": "https://ssd.eff.org/en/module/communicating-others"
     }
   },
-  "an": {
-    "tmodel": {
-      "en": "https://www.accessnow.org/first-look-at-digital-security/"
-    }
-  },
-  "ttech": {
-    "matrices": {
-      "en": "https://holistic-security.tacticaltech.org/chapters/explore/2-8-identifying-and-analysing-threats.html"
-    },
-    "detox": {
-      "en": "https://datadetoxkit.org/en/home"
-    }
-  },
-  "nist":{
-    "framework": {
-      "en": "https://www.nist.gov/cyberframework"
-    }
-  },
-  "mitre": {
-    "matrix": {
-      "en": "https://attack.mitre.org/"
-    },
-    "tabletop": {
-      "en": "https://www.mitre.org/sites/default/files/publications/pr_14-3929-cyber-exercise-playbook.pdf"
-    }
-  },
-  "cat": {
-    "irplan": {
-      "en": "https://docs.google.com/document/d/1wf4il1JI5Bm7kI2JVo1diUv-v4A_a6Fk59NeNtrgoiA/edit?usp=sharing"
-    }
-  },
-  "soap": {
-    "tool": {
-      "en": "https://usesoap.app/"
-    }
-  },
-  "sans": {
-    "templates": {
-      "en": "https://www.sans.org/information-security-policy/"
-    },
-    "endpoint": {
-      "en": "https://www.sans.org/white-papers/39900/"
-    }
-  },
-  "carbide": {
-    "windows": {
-      "en": "https://carbidesecure.com/resources/security-best-practices-hardening-windows-10/"
-    },
-    "mac": {
-      "en": "https://carbidesecure.com/resources/our-best-practices-for-securing-your-macbook/"
-    }
-  },
-  "prey": {
+  "element": {
     "web": {
-      "en": "https://preyproject.com/"
+      "en": "https://element.io/communities"
     }
   },
-  "moodle": {
-    "web": {
-      "en": "https://moodle.org/"
+  "equalite": {
+    "deflect": {
+      "en": "https://equalit.ie/portfolio/deflect/"
     }
   },
-  "ilias": {
-    "web": {
-      "en": "https://www.ilias.de/en/"
-    }
-  },
-  "learndash": {
-    "web": {
-      "en": "https://www.learndash.com/"
-    }
-  },
-  "docebo": {
-    "web": {
-      "en": "https://www.docebo.com/"
-    }
-  },
-  "totem": {
-    "web": {
-      "en": "https://totem-project.org/"
-    }
-  },
-  "soak": {
-    "web": {
-      "en": "https://spideroak.com/one/"
-    }
-  },
-  "tresorit": {
-    "web": {
-      "en": "https://tresorit.com/"
-    }
-  },
-  "nextcloud": {
-    "web": {
-      "en": "https://nextcloud.com/"
-    }
-  },
-  "ocloud": {
-    "web": {
-      "en": "https://owncloud.com/"
-    }
-  },
-  "cmator": {
-    "web": {
-      "en": "https://cryptomator.org/"
-    }
-  },
-  "vera": {
-    "web": {
-      "en": "https://www.veracrypt.fr/en/Home.html"
-    }
-  },
-  "hibp": {
-    "web": {
-      "en": "https://haveibeenpwned.com/"
-    }
-  },
-  "1pass": {
-    "biz": {
-      "en": "https://1password.com/business/"
-    },
-    "discount": {
-      "en": "https://1password.community/discussion/87059/teams-schools-and-other-nonprofits"
-    }
-  },
-  "lpass": {
-    "web": {
-      "en": "https://www.lastpass.com/enterprise/enterprise-features"
-    }
-  },
-  "dlane": {
-    "web": {
-      "en": "https://www.dashlane.com/"
-    }
-  },
-  "authy": {
-    "web": {
-      "en": "https://authy.com/features/"
-    }
-  },
-  "kpassxc": {
-    "web": {
-      "en": "https://keepassxc.org/"
-    }
-  },
-  "keeper": {
-    "web": {
-      "en": "https://www.keepersecurity.com/"
-    }
-  },
-  "bwarden": {
-    "web": {
-      "en": "https://bitwarden.com/"
+  "fb": {
+    "journos": {
+      "en": "https://www.facebook.com/journalismproject/journalist-registration-facebook"
     }
   },
   "google": {
@@ -194,168 +209,9 @@ var links = {
       "en": "https://www.getoutline.org/"
     }
   },
-  "cisco": {
-    "webex": {
-      "en":"https://www.webex.com/"
-    }
-  },
-  "trailbits": {
-    "algo": {
-      "en": "https://blog.trailofbits.com/2016/12/12/meet-algo-the-vpn-that-works/"
-    }
-  },
-  "creports": {
-    "vpns": {
-      "en": "https://www.consumerreports.org/vpn-services/should-you-use-a-vpn-a5562069524/"
-    }
-  },
-  "psiphon": {
-    "web": {
-      "en": "http://psiphon.ca"
-    }
-  },
-  "lantern": {
-    "web": {
-      "en": "https://getlantern.org/"
-    }
-  },
-  "bmask": {
-    "web": {
-      "en": "https://bitmask.net"
-    }
-  },
-  "mvad": {
-    "web": {
-      "en": "https://www.tunnelbear.com/ngo-support-network"
-    }
-  },
-  "tbear": {
-    "web": {
-      "en": "https://www.tunnelbear.com/ngo-support-network"
-    }
-  },
-  "nyt": {
-    "vpns": {
-      "en": "https://www.nytimes.com/wirecutter/reviews/best-vpn-service/"
-    }
-  },
-  "deleteme": {
-    "web": {
-      "en": "https://joindeleteme.com/"
-    }
-  },
-  "lifelock": {
-    "web": {
-      "en": "https://www.nortonlifelock.com/us/en/"
-    }
-  },
-  "dnstwist": {
-    "web": {
-      "en": "https://dnstwist.it/"
-    }
-  },
-  "coral": {
-    "web": {
-      "en": "https://coralproject.net/"
-    }
-  },
-  "perapi": {
-    "web": {
-      "en": "https://www.perspectiveapi.com/#/home"
-    }
-  },
-  "bparty": {
-    "web": {
-      "en": "https://www.blockpartyapp.com/"
-    }
-  },
-  "tpoppy": {
-    "web": {
-      "en": "http://TallPoppy.io"
-    }
-  },
-  "ctangle": {
-    "web": {
-      "en": "https://www.crowdtangle.com/"
-    }
-  },
-  "smat": {
-    "kit": {
-      "en": "https://social-media-analysis-toolkit.herokuapp.com/"
-    }
-  },
-  "pduck": {
-    "web": {
-      "en": "https://www.privacyduck.com/"
-    }
-  },
-  "babelst": {
-    "web": {
-      "en": "https://www.babelstreet.com/"
-    }
-  },
-  "repdef": {
-    "web": {
-      "en": "https://www.reputationdefender.com/"
-    }
-  },
-  "fb": {
-    "journos": {
-      "en": "https://www.facebook.com/journalismproject/journalist-registration-facebook"
-    }
-  },
-  "miradore": {
-    "web": {
-      "en": "https://www.miradore.com/device-management/mobile-device-management-mdm/"
-    }
-  },
-  "3ma": {
-    "web": {
-      "en": "https://threema.ch/en"
-    }
-  },
-  "pmail": {
-    "web": {
-      "en": "https://protonmail.com/"
-    }
-  },
-  "vmware": {
-    "player": {
-      "en": "https://www.vmware.com/products/workstation-player.html"
-    },
-    "virtualization": {
-      "en":"https://www.infoq.com/articles/virtualization-saas/"
-    }
-  },
-  "vbox": {
-    "web": {
-      "en": "https://www.virtualbox.org/"
-    }
-  },
-  "cflare": {
-    "ddos": {
-      "en": "https://www.cloudflare.com/learning/ddos/what-is-a-ddos-attack/"
-    },
-    "gal": {
-      "en": "https://www.cloudflare.com/galileo/"
-    },
-    "https": {
-      "en": "https://www.cloudflare.com/learning/ssl/what-is-https/"
-    }
-  },
-  "equalite": {
-    "deflect": {
-      "en": "https://equalit.ie/portfolio/deflect/"
-    }
-  },
-  "2fa": {
-    "web": {
-      "en": "http://twofactorauth.org"
-    }
-  },
-  "wfence": {
-    "web": {
-      "en": "https://www.wordfence.com/"
+  "guardian": {
+    "ss7": {
+      "en": "https://www.theguardian.com/technology/2016/apr/19/ss7-hack-explained-mobile-phone-vulnerability-snooping-texts-calls"
     }
   },
   "gproj": {
@@ -363,49 +219,14 @@ var links = {
       "en": "https://guardianproject.info/apps/org.havenapp.main/"
     }
   },
-  "synology": {
-    "station": {
-      "en": "https://www.synology.com/en-us/surveillance"
-    }
-  },
   "haicam": {
     "web": {
       "en": "https://haicam.tech/"
     }
   },
-  "aclu": {
-    "stingrays": {
-      "en": "https://www.aclu.org/issues/privacy-technology/surveillance-technologies/stingray-tracking-devices-whos-got-them"
-    }
-  },
-  "guardian": {
-    "ss7": {
-      "en": "https://www.theguardian.com/technology/2016/apr/19/ss7-hack-explained-mobile-phone-vulnerability-snooping-texts-calls"
-    }
-  },
-  "signal": {
+  "hibp": {
     "web": {
-      "en": "http://signal.org"
-    }
-  },
-  "wire": {
-    "web": {
-      "en": "http://wire.com"
-    }
-  },
-  "mmost": {
-    "web": {
-      "en": "http://mattermost.com"
-    }
-  },
-  "element": {
-    "web": {
-      "en": "https://element.io/communities"
-    }
-  },
-  "icnl": {
-    "web": {
-      "en": "https://www.icnl.org/resources#resourcecollections"
+      "en": "https://haveibeenpwned.com/"
     }
   },
   "iba": {
@@ -413,29 +234,90 @@ var links = {
       "en": "https://www.ibanet.org/"
     }
   },
-  "yubico": {
-    "key": {
-      "en": "https://www.yubico.com/"
-    }
-  },
-  "solo": {
-    "key": {
-      "en": "https://solokeys.com/"
-    }
-  },
-  "titan": {
-    "key": {
-      "en": "https://store.google.com/us/product/titan_security_key"
-    }
-  },
-  "duo": {
+  "icnl": {
     "web": {
-      "en": "https://duo.com/"
+      "en": "https://www.icnl.org/resources#resourcecollections"
     }
   },
-  "okta": {
+  "ilias": {
     "web": {
-      "en": "https://www.okta.com/products/single-sign-on/"
+      "en": "https://www.ilias.de/en/"
+    }
+  },
+  "istorageuk": {
+    "web": {
+      "en": "https://istorage-uk.com/"
+    }
+  },
+  "keeper": {
+    "web": {
+      "en": "https://www.keepersecurity.com/"
+    }
+  },
+  "kpassxc": {
+    "web": {
+      "en": "https://keepassxc.org/"
+    }
+  },
+  "lantern": {
+    "web": {
+      "en": "https://getlantern.org/"
+    }
+  },
+  "learndash": {
+    "web": {
+      "en": "https://www.learndash.com/"
+    }
+  },
+  "libre"{
+    "office": {
+      "en": "https://www.libreoffice.org/"
+    }
+  }
+  "lifelock": {
+    "web": {
+      "en": "https://www.nortonlifelock.com/us/en/"
+    }
+  },
+  "lpass": {
+    "web": {
+      "en": "https://www.lastpass.com/enterprise/enterprise-features"
+    }
+  },
+  "mention": {
+    "web": {
+      "en": "https://mention.com/en/"
+    }
+  },
+  "miradore": {
+    "web": {
+      "en": "https://www.miradore.com/device-management/mobile-device-management-mdm/"
+    }
+  },
+  "mitre": {
+    "matrix": {
+      "en": "https://attack.mitre.org/"
+    },
+    "tabletop": {
+      "en": "https://www.mitre.org/sites/default/files/publications/pr_14-3929-cyber-exercise-playbook.pdf"
+    }
+  },
+  "mmost": {
+    "web": {
+      "en": "http://mattermost.com"
+    }
+  },
+  "moodle": {
+    "web": {
+      "en": "https://moodle.org/"
+    }
+  },
+  "mozilla": {
+    "tbird": {
+      "en": "https://www.thunderbird.net/en-US/"
+    },
+    "doh": {
+      "en": "https://support.mozilla.org/en-US/kb/firefox-dns-over-https"
     }
   },
   "ms": {
@@ -452,12 +334,59 @@ var links = {
       "en": "https://www.microsoft.com/en-us/account/authenticator"
     }
   },
-  "apple": {
-    "filevault": {
-      "en": "https://support.apple.com/en-us/HT204837"
-    },
-    "management": {
-      "en": "https://www.apple.com/business/essentials/"
+  "mvad": {
+    "web": {
+      "en": "https://www.tunnelbear.com/ngo-support-network"
+    }
+  },
+  "nextcloud": {
+    "web": {
+      "en": "https://nextcloud.com/"
+    }
+  },
+  "nist":{
+    "framework": {
+      "en": "https://www.nist.gov/cyberframework"
+    }
+  },
+  "nyt": {
+    "vpns": {
+      "en": "https://www.nytimes.com/wirecutter/reviews/best-vpn-service/"
+    }
+  },
+  "ocloud": {
+    "web": {
+      "en": "https://owncloud.com/"
+    }
+  },
+  "okta": {
+    "web": {
+      "en": "https://www.okta.com/products/single-sign-on/"
+    }
+  },
+  "pduck": {
+    "web": {
+      "en": "https://www.privacyduck.com/"
+    }
+  },
+  "perapi": {
+    "web": {
+      "en": "https://www.perspectiveapi.com/#/home"
+    }
+  },
+  "pmail": {
+    "web": {
+      "en": "https://protonmail.com/"
+    }
+  },
+  "prey": {
+    "web": {
+      "en": "https://preyproject.com/"
+    }
+  },
+  "psiphon": {
+    "web": {
+      "en": "http://psiphon.ca"
     }
   },
   "redhat": {
@@ -465,17 +394,52 @@ var links = {
       "en": "https://access.redhat.com/solutions/100463"
     }
   },
-  "bitdefender": {
-    "gzultra": {
-      "en": "https://www.bitdefender.com/business/enterprise-products/ultra-security.html"
+  "repdef": {
+    "web": {
+      "en": "https://www.reputationdefender.com/"
     }
   },
-  "mozilla": {
-    "tbird": {
-      "en": "https://www.thunderbird.net/en-US/"
+  "sans": {
+    "templates": {
+      "en": "https://www.sans.org/information-security-policy/"
     },
-    "doh": {
-      "en": "https://support.mozilla.org/en-US/kb/firefox-dns-over-https"
+    "endpoint": {
+      "en": "https://www.sans.org/white-papers/39900/"
+    }
+  },
+  "signal": {
+    "web": {
+      "en": "http://signal.org"
+    }
+  },
+  "smat": {
+    "kit": {
+      "en": "https://social-media-analysis-toolkit.herokuapp.com/"
+    }
+  },
+  "soak": {
+    "web": {
+      "en": "https://spideroak.com/one/"
+    }
+  },
+  "soap": {
+    "tool": {
+      "en": "https://usesoap.app/"
+    }
+  },
+  "solo": {
+    "key": {
+      "en": "https://solokeys.com/"
+    }
+  },
+  "synology": {
+    "station": {
+      "en": "https://www.synology.com/en-us/surveillance"
+    }
+  },
+  "tbear": {
+    "web": {
+      "en": "https://www.tunnelbear.com/ngo-support-network"
     }
   },
   "techsoup": {
@@ -483,19 +447,42 @@ var links = {
       "en": "https://www.techsoup.org/"
     }
   },
+  "titan": {
+    "key": {
+      "en": "https://store.google.com/us/product/titan_security_key"
+    }
+  },
+  "totem": {
+    "web": {
+      "en": "https://totem-project.org/"
+    }
+  },
   "tor": {
     "browser": {
       "en": "https://tb-manual.torproject.org/about/"
     }
   },
-  "mention": {
+  "tpoppy": {
     "web": {
-      "en": "https://mention.com/en/"
+      "en": "http://TallPoppy.io"
     }
   },
-  "ctower": {
+  "trailbits": {
+    "algo": {
+      "en": "https://blog.trailofbits.com/2016/12/12/meet-algo-the-vpn-that-works/"
+    }
+  },
+  "tresorit": {
     "web": {
-      "en": "https://changetower.com/"
+      "en": "https://tresorit.com/"
+    }
+  },
+  "ttech": {
+    "matrices": {
+      "en": "https://holistic-security.tacticaltech.org/chapters/explore/2-8-identifying-and-analysing-threats.html"
+    },
+    "detox": {
+      "en": "https://datadetoxkit.org/en/home"
     }
   },
   "twalker": {
@@ -503,24 +490,37 @@ var links = {
       "en": "https://www.talkwalker.com/"
     }
   },
-  "apache": {
-    "oofice": {
-      "en": "https://www.openoffice.org/"
-    }
-  },
-  "apricorn": {
+  "vbox": {
     "web": {
-      "en": "https://apricorn.com/"
+      "en": "https://www.virtualbox.org/"
     }
   },
-  "istorageuk": {
+  "vera": {
     "web": {
-      "en": "https://istorage-uk.com/"
+      "en": "https://www.veracrypt.fr/en/Home.html"
     }
   },
-  "libre"
-    "office": {
-      "en": "https://www.libreoffice.org/"
+  "vmware": {
+    "player": {
+      "en": "https://www.vmware.com/products/workstation-player.html"
+    },
+    "virtualization": {
+      "en":"https://www.infoq.com/articles/virtualization-saas/"
+    }
+  },
+  "wfence": {
+    "web": {
+      "en": "https://www.wordfence.com/"
+    }
+  },
+  "wire": {
+    "web": {
+      "en": "http://wire.com"
+    }
+  },
+  "yubico": {
+    "key": {
+      "en": "https://www.yubico.com/"
     }
   },
 }
