071fc9fb0ae859ce36742257d84152771413e803
Gem Barrett <gembarrett@users.noreply.github.com>
2022-04-04 13:39:12 -0400

    removing old files and comments

--- assets/js/controllers/policyPage.js
+++ assets/js/controllers/policyPage.js
@@ -26,29 +26,6 @@ controllers.policyPage = function(data, params){
     }
   }
 
-  // title of page: resText.title
-  // content match: tally[key] to match resText.content[X].area
-  // title of area: resText.content[X].title
-  // area intro: resText.content[X].general.why
-    // loop through array and add items between <p> tags
-  // area example: resText.content[X].general.eg
-  // scoretext: tally[key] adds to resText.content[X].score
-  // conditional on score:
-    // if tally[key] is < 10
-      // set level to resText.content[X].results.exceeds
-    // if tally[key] is < 20
-      // set level to resText.content[X].results.ok
-    // if tally[key] is > 20
-      // set level to resText.content[X].results.below
-
-    // for whatever level, grab
-      // reusables.(level).title
-      // reusables.(level).content
-
-  // when all answers are processed, loop through the array
-  // for each object, compare the tallied points to the area's limits
-  // push the relevant content to the object
-
   // pass this array to the template
   var policyContainer = templates.policyTemplate(tally);
   utils.render('page', policyContainer);
