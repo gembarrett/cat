667fa9d54ff4ab04457651d502f7fd484c2758db
Gem Barrett <gembarrett@users.noreply.github.com>
2022-04-22 17:32:15 -0400

    finishing up home page desktop styling

--- assets/js/templates/trio.js
+++ assets/js/templates/trio.js
@@ -1,14 +1,14 @@
 templates.trio = function(data){
   var text = en_oc;
   var titles = [text.team.title, text.legal.title, text.process.title];
-  console.log(titles);
+  var colors = ['brown', 'yellow', 'green'];
   var cols = '';
   for (var i = 3; i > 0; i--) {
-    cols += `<div class="trio-3">`;
+    cols += `<div class="trio-col">`;
     cols += `<img src="assets/images/`+i+`-ombre.png">`;
     cols += `<img src="assets/images/`+i+`-vertical.png">`;
-    cols += `<h2 class="title">`+titles[i-1]+`</h2></div>`;
+    cols += `<h2 class="title bg-`+colors[i-1]+`">`+titles[i-1]+`</h2></div>`;
   }
-  var content = `<div id="trio" class="contain">`+cols+`</div>`;
+  var content = `<div id="trio">`+cols+`</div>`;
     return content;
 };
