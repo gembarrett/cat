ed83b6a5fccc530e371c88326f8ac70957ab0ac9
Gem Barrett <gembarrett@users.noreply.github.com>
2022-04-27 12:56:29 -0400

    added wrappers to basics text

--- assets/js/templates/legal.js
+++ assets/js/templates/legal.js
@@ -2,7 +2,7 @@ templates.legal = function(data){
   var content = `<div id="legal" class="format">
                   <h1 class="title">`+data.legal.title+`</h1>`;
 
-  content = parseContent(data.legal.content, content);
+  content = parseContent(data.legal.content, content, 'salford-text');
 
   content += `<button class="btn btn-prim title format">`+data.ux.content[0].text+`</button></div>`;
   return content;
--- assets/js/templates/team.js
+++ assets/js/templates/team.js
@@ -1,11 +1,11 @@
 templates.team = function(data){
   var content = `<div id="team" class="format">
-                <h1 class="title">`+data.team.title+`</h1>`;
+                <h1 class="title">`+data.team.title+`</h1><div class="salford-text">`;
 
   for (var t = 0; t < data.team.content.length; t++) {
     content += `<p><span>`+data.team.content[t].name+`</span>`+ data.team.content[t].p+`</p>`;
   }
 
-  content += `<button class="btn btn-prim title format">`+data.ux.content[0].text+`</button></div>`;
+  content += `</div><button class="btn btn-prim title format">`+data.ux.content[0].text+`</button></div>`;
   return content;
 };
