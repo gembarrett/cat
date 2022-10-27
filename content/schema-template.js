6e34ae3b408d783f47d508ce93390c75bec180e4
Gem Barrett <gembarrett@users.noreply.github.com>
2022-10-27 19:08:02 -0400

    updating the schema template in prep for the new content

--- content/schema-template.js
+++ content/schema-template.js
@@ -1,24 +1,48 @@
 {
-  "level": "intro", // which of the three levels do these questions belong to
-  "questions": [ // contains this level's questions
-  {
-    "q": "Question placeholder goes here", // what's the question text
-    "area": "a", // which category should the score be added to
-    "answers": [ // contains this question's answers
+  "section": "understand-risk", // which of the sections do these questions belong to
+  "subs": [
+    "your-org": [
+      "questions": [ // contains this level's questions
       {
-        "a": "Answer placeholder goes here", // what's the answer text
-        "pts": 10 // how many points should be added to the category
-      },
-      {
-        "a": "Next answer placeholder goes here",
-        "pts": 15
-      },
-      {
-        "a": "Last answer placeholder goes here",
-        "pts": 0
+        "q": "Question placeholder goes here", // what's the question text
+        "area": "a", // which category should the score be added to
+        "answers": [ // contains this question's answers
+          {
+            "a": "Answer placeholder goes here", // what's the answer text
+            "pts": 10 // how many points should be added to the category
+          },
+          {
+            "a": "Next answer placeholder goes here",
+            "pts": 15
+          },
+          {
+            "a": "Last answer placeholder goes here",
+            "pts": 0
+          }
+        ]
       }
     ]
-  },
+  ],
+  "external-threats": [
+    {
+      "q": "Question placeholder goes here", // what's the question text
+      "area": "a", // which category should the score be added to
+      "answers": [ // contains this question's answers
+        {
+          "a": "Answer placeholder goes here", // what's the answer text
+          "pts": 10 // how many points should be added to the category
+        },
+        {
+          "a": "Next answer placeholder goes here",
+          "pts": 15
+        },
+        {
+          "a": "Last answer placeholder goes here",
+          "pts": 0
+        }
+      ]
+    }
+  ]
 }
 
 
