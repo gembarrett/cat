011ba2033faeda8d7fde9edb103dba38079be6e3
Gem Barrett <gembarrett@users.noreply.github.com>
2023-12-17 22:56:19 -0500

    quick wording change

--- content/en-content.js
+++ content/en-content.js
@@ -2060,7 +2060,7 @@ var en_oc = {
            "result": {
                "title": "Save your results and recommendations",
                "content": "Please copy the link below and save it in a safe place. You can use this link any time within the next 30 days to resume answering questions on the form.",
-               "disclaimer": "Without the link, the data you have entered cannot be retrieved, and you will have to start filling out the form at the beginning."
+               "disclaimer": "Without the link, the data you have entered cannot be retrieved, and you will have to start from the beginning."
            },
            "email": {
                "title": "Email Your Results and Recommendations",
