ed04332ab5e3599b5e707ec3b1e7c2aae7b4006c
Gem Barrett <gembarrett@users.noreply.github.com>
2024-03-23 15:00:43 -0400

    updated with loading content

--- assets/js/templates/legal.js
+++ assets/js/templates/legal.js
@@ -2,7 +2,7 @@ templates.legal = function(data){
   var disc = '';
   disc = parseContent(data.content, disc);
   var legalContent = `<div class="brown-bg"><div class="contain-sm add-shadow">
-                      <h2\>`+data.title+`</h2>`
+                      <h2>`+data.title+`</h2>`
                       +disc+`</div></div>`;
   return legalContent;
 };
